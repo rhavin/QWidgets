891c2889ea992cc6b5348524df31eb67bed7f77e
rhavin <rhavin@shadowtec.de>
2025-08-26 19:56:48 +0200

    4

--- QWidgets.php
+++ QWidgets.php
@@ -4,7 +4,7 @@ namespace Q\Widgets;
 /**
  * Plugin Name:        QWidgets
  * Plugin URI:         https://github.com/rhavin/QWidgets
- * Version:            0.0.3
+ * Version:            0.0.4
  * Description:        Description
  * Author:             rhavin
  * Author URI:         https://rhavin.de/
